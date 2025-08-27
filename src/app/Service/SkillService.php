a9799148ad63dae9ce158b9ea8cb86b09e3253da
Kobietakoala <kobietakoala@gmail.com>
2025-08-27 15:32:03 +0200

    Task#86c51devn Add `SkillService` and register it in `AppServiceProvider`
    - Create `SkillService` with a method to fetch skill categories along with their details and associated skills.
    - Register `SkillService` as a singleton in `AppServiceProvider`.

---
+++ src/app/Service/SkillService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Service;
+
+use App\Models\SkillCategory;
+
+class SkillService
+{
+    public function getCategoriesWithSkills(): array
+    {
+        return $this->fetchCategoriesWithSkills();
+    }
+
+    private function fetchCategoriesWithSkills(): array
+    {
+        $categories = SkillCategory::all();
+
+        return $categories->map(function (SkillCategory $category) {
+            return [
+                'id' => $category->getAttribute('id'),
+                'name' => is_array($category->name) ?
+                    $category->getTranslation('name', app()->getLocale()) :
+                    $category->getAttribute('name'),
+                'logo' => $category->getAttribute('logo') ? [
+                    'id' => $category->getAttribute('logo')->id,
+                    'filename' => $category->getAttribute('logo')->filename,
+                    'url' => $category->getAttribute('logo')->getUrlAttribute(),
+                    'mime_type' => $category->getAttribute('logo')->mime_type,
+                ] : null,
+                'skills' => $category->getSkills(),
+            ];
+        })->toArray();
+    }
+
+}
