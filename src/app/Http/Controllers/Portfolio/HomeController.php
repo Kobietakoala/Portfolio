9ef6841309cd68f9f1bc89ac3b66c7483f489fc0
Kobietakoala <kobietakoala@gmail.com>
2025-08-27 10:20:58 +0200

    Task#86c51deqh Integrate profile data into HomeController and header view
    - Inject `ProfileService` into `HomeController` for retrieving profile data.
    - Update HomeController `index` method to pass profile data to the home view.
    - Redesign `header.blade.php` to dynamically display profile information, including avatar, name, and position.

--- src/app/Http/Controllers/Portfolio/HomeController.php
+++ src/app/Http/Controllers/Portfolio/HomeController.php
@@ -6,17 +6,23 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Portfolio;
 
 use App\Http\Controllers\Controller;
+use App\Service\ProfileService;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Routing\Attribute\Route;
 
 class HomeController extends Controller
 {
+    public function __construct(
+        private readonly ProfileService $profileService
+    ) { }
+
     /**
      * @return Renderable
      */
     #[Route('/', methods: ['GET'], name: 'home')]
     public function index()
     {
-        return view('portfolio.home');
+        $profileData = $this->profileService->getProfileData();
+        return view('portfolio.home', compact('profileData'));
     }
 }
